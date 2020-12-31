781384bdbc3af2f490d5e30356fbf91b3d9a282e
Ekaterina Zhigalkina <ekaterina.zhigalkina@kelnik.ru>
2020-12-31 11:33:34 +0300

    sp ny

--- README.md
+++
@@ -1,1 +0,0 @@
-# spboard.github.io
---
+++ index.js
@@ -0,0 +1,39 @@
+console.log('Made in https://planoplan.com/ 💙');
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.gallery) {
+    window.gallery.init();
+  }
+
+  var button = document.getElementById('audio');
+  var audio = document.getElementById('music');
+  var isAudioStarted = false;
+  var isAudioPlay = false;
+  var events = ['mousedown', 'keydown'];
+
+  function autoStartAudio() {
+    if (isAudioStarted) return;
+
+    isAudioStarted = true;
+    isAudioPlay = true;
+    audio.play();
+
+    events.forEach(function (event) {
+      window.removeEventListener(event, autoStartAudio);
+    });
+  }
+
+  events.forEach(function (event) {
+    window.addEventListener(event, autoStartAudio);
+  });
+
+  button.addEventListener('click', function () {
+    if (isAudioPlay) {
+      audio.pause();
+      isAudioPlay = false;
+    } else {
+      audio.play();
+      isAudioPlay = true;
+    }
+  });
+});
